24e2dc3cdbac04f31c942be42b0a5a9821a8e690
Israel Huallpara <israelhuallpara@gmail.com>
2023-02-13 02:52:47 -0400

    Conclusión de la página web en construcción

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		{transform: (content) => content.replace(/taos:/g, '')},
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('taos/plugin')
+	],
+	safelist: [
+		'!duration-0',
+		'!delay-0',
+		'html.js :where([class*="taos:"]:not(.taos-init))'
+	],
 }
